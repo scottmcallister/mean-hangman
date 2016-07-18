2c5740f16141a86983ff99fe11a56d9cd263e930
Scott McAllister <scottm@nextopia.com>
2016-07-18 10:58:57 +0000

    use mongolab in production

--- server/config/db.conf.js
+++ server/config/db.conf.js
@@ -3,7 +3,7 @@ import dbConst from '../constants/db.json';
 
 export default class DBConfig {
     static init() {
-      const URL = (process.env.NODE_ENV === 'production') ? process.env.MONGOHQ_URL
+      const URL = (process.env.NODE_ENV === 'production') ? process.env.MONGOLAB_URI
                                                           : dbConst.localhost;
 
       mongoose.connect(URL);
